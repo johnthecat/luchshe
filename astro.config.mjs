b9015d50133d34a19e7b0cfe940e01a69ce1c3a1
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-06-01 13:35:47 +0300

    Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://johnthecat.github.io',
-    base: '/luchshe',
+  site: 'https://johnthecat.github.io',
+  base: '/luchshe',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
